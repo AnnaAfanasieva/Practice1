d02b2bbbad8debe6f9caccae2f3d3a3c241750e0
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-09-14 23:11:35 +0300

    Part4 and Part4Test complete

--- src/main/java/my/practice1/Part4.java
+++ src/main/java/my/practice1/Part4.java
@@ -1,7 +1,20 @@
 package my.practice1;
 
 public class Part4 {
-    public static void main(String[] args) {
+    static int result;
 
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        result = nod(num1, num2);
+        System.out.println(result);
+    }
+
+    private static int nod(int num1, int num2) {
+        while (num1 != 0 && num2 != 0) {
+            if (num1 > num2) num1 = num1 % num2;
+            else num2 = num2 % num1;
+        }
+        return num1 + num2;
     }
 }
--- src/test/java/my/practice1/Part4Test.java
+++ src/test/java/my/practice1/Part4Test.java
@@ -1,4 +1,12 @@
 package my.practice1;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Part4Test {
+    @Test
+    public void testPart4() {
+        Part4.main(new String[]{"25", "15"});
+        Assert.assertEquals(5, Part4.result);
+    }
 }
