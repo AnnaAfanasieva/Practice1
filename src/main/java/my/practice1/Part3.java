d1e2ab3aa5808a9db5a773a5550454e33b408ef4
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-09-14 15:52:05 +0300

    Part3 and Part3Test complete

--- src/main/java/my/practice1/Part3.java
+++ src/main/java/my/practice1/Part3.java
@@ -2,6 +2,10 @@ package my.practice1;
 
 public class Part3 {
     public static void main(String[] args) {
-
+        int indexOfFinalElement = args.length-1;
+        for (int i=0; i<indexOfFinalElement; i++){
+            System.out.print(args[i]+" ");
+        }
+        System.out.print(args[indexOfFinalElement]);
     }
 }
--- src/test/java/my/practice1/Part3Test.java
+++ src/test/java/my/practice1/Part3Test.java
@@ -1,4 +1,10 @@
 package my.practice1;
 
+import org.junit.Test;
+
 public class Part3Test {
+    @Test
+    public void testPart3(){
+        Part3.main(new String[]{"a", "b", "c", "d", "e", "f"});
+    }
 }
