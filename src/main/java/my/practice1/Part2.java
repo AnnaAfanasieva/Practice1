90f6af7dfa8a9b1d97c703c3aab469e949f005ea
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-09-14 15:32:31 +0300

    Part2 and Part2Test complete

--- src/main/java/my/practice1/Part2.java
+++ src/main/java/my/practice1/Part2.java
@@ -2,6 +2,6 @@ package my.practice1;
 
 public class Part2 {
     public static void main(String[] args) {
-
+        System.out.print(Integer.parseInt(args[0])+Integer.parseInt(args[1]));
     }
 }
--- src/test/java/my/practice1/Part2Test.java
+++ src/test/java/my/practice1/Part2Test.java
@@ -1,4 +1,10 @@
 package my.practice1;
 
+import org.junit.Test;
+
 public class Part2Test {
+    @Test
+    public void testPart2(){
+        Part2.main(new String[]{"7", "15"});
+    }
 }
