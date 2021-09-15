9f907ae9e3fc688170d8f9689784894c368a344a
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-09-15 13:51:13 +0300

    Part5 and Part5Test complete

--- src/test/java/my/practice1/Part5Test.java
+++ src/test/java/my/practice1/Part5Test.java
@@ -1,4 +1,12 @@
 package my.practice1;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Part5Test {
+    @Test
+    public void testPart5() {
+        Part5.main(new String[]{"256504552"});
+        Assert.assertEquals(34, Part5.result);
+    }
 }
