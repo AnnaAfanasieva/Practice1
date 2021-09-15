53f536ef33b6b0644b88fcd7a279549e1c61f407
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-09-15 18:24:20 +0300

    Part5 and Part5Test complete

--- src/main/java/my/practice1/Part5.java
+++ src/main/java/my/practice1/Part5.java
@@ -1,7 +1,19 @@
 package my.practice1;
 
 public class Part5 {
-    public static void main(String[] args) {
+    static int result;
 
+    public static void main(String[] args) {
+        result = sumOfNumbers(Integer.parseInt(args[0]));
+        System.out.print(result);
+    }
+
+    private static int sumOfNumbers(int number) {
+        int result = 0;
+        while (number > 0) {
+            result += number % 10;
+            number /= 10;
+        }
+        return result;
     }
 }
