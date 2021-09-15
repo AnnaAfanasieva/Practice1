d1307550031e833a0ad5c68864b6b183f6c1a0e6
AnnaAfanasieva <aniafanasieva24@gmail.com>
2021-09-15 17:50:39 +0300

    Revert "Part5 and Part5Test complete"
    This reverts commit 9f907ae9e3fc688170d8f9689784894c368a344a.

--- src/main/java/my/practice1/Part5.java
+++ src/main/java/my/practice1/Part5.java
@@ -1,19 +1,7 @@
 package my.practice1;
 
 public class Part5 {
-    static int result;
-
     public static void main(String[] args) {
-        result = sumOfNumbers(Integer.parseInt(args[0]));
-        System.out.print(result);
-    }
 
-    private static int sumOfNumbers(int number) {
-        int result = 0;
-        while (number > 0) {
-            result += number % 10;
-            number /= 10;
-        }
-        return result;
     }
 }
